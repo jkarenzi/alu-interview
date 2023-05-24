3eaeeb1f92e2c301218c73d1ebe4e372b253af18
jkarenzi <j.karenzi@alustudent.com>
2023-05-24 11:32:21 +0200

    added minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""minimum operations"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n = n // divisor
+        else:
+            divisor += 1
+
+    return operations
